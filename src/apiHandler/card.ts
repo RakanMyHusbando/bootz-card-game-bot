97041c23e079fb5a8c9e700d2bd2abb69f3d0531
hallo002f <witt.clemens@gmx.de>
2024-12-11 10:33:51 +0100

    feat: added reveal command

--- src/apiHandler/card.ts
+++ src/apiHandler/card.ts
@@ -28,3 +28,23 @@ export const GetCardById = async (id: number): Promise<InterCard | null> => {
             return null;
         });
 };
+
+export const GetRandomCard = async (): Promise<InterCard | null> => {
+    const reqAllCard = new Request("http://localhost:5000/cards/randomCard/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return await fetch(reqAllCard)
+        .then((res) => res.json())
+        .then((res) =>
+            res.status === 200 && res.data && res.data.length != 0
+                ? (res.data[0] as InterCard)
+                : null,
+        )
+        .catch((err) => {
+            console.error(err);
+            return null;
+        });
+};
